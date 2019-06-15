5c9e0bf2acb6e1255ade7a6451b6a6c249ff2855
Andres Felipe <sandovalfenix@hotmail.com>
2019-06-14 20:54:34 -0500

    1906142053
    Instalacion de Yanic v3.0

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+date_default_timezone_set('America/Bogota');
+
+use app\Autoload;
+use app\config\Request;
+use app\config\Router;
+
+define('__DS__', DIRECTORY_SEPARATOR);
+define('__ROOT__', realpath(dirname(__FILE__)) . __DS__);
+
+include_once(__ROOT__ . 'app/Autoload.php');
+include_once(__ROOT__ . 'vendor/autoload.php');
+
+Autoload::run();
+$request = new Request();
+new Router($request);
